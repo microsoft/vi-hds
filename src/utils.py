65fb3078c35cff161e7b813457e1a3e323628deb
Neil Dalchau <ndalchau@microsoft.com>
2020-02-19 07:43:09 +0000

    Turning off tensorboard histograms by default

--- src/utils.py
+++ src/utils.py
@@ -90,7 +90,7 @@ def apply_defaults(spec):
         'init_latent_species' : 0.001,
         'transfer_func' : tf.nn.tanh,
         'n_hidden_decoder_precisions' : 20,
-        'plot_histograms' : True
+        'plot_histograms' : False
     }
     for k in spec:
         params[k] = spec[k]
